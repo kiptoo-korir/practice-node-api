3325736bac9c9f14d56af45479dc49bbdd1125d4
Elijah Korir <elijahkiptoo98@gmail.com>
2021-11-03 22:08:23 +0300

    Register method added to authController

--- modules/auth/authController.js
+++ modules/auth/authController.js
@@ -1,1 +1,11 @@
-async function register(user) {}
+const { createUser } = require("./authDAL");
+
+async function register(user) {
+  try {
+    const userArray = Object.values(user);
+    const newUser = await createUser(userArray);
+    // Once user is created, create token for user
+  } catch (error) {
+    throw error;
+  }
+}

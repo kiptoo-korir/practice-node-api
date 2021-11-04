7c3c7f5b465b6d8bf1233ef64bf8167b43c609db
Elijah Korir <elijahkiptoo98@gmail.com>
2021-11-04 09:38:19 +0300

    Api Router added

--- bin/app.js
+++ bin/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { port } = require("../utils/config");
 const { router: eventRouter } = require("../modules/events");
+const { router: authRouter } = require("../modules/auth");
 
 const hostPort = port || 3000;
 
@@ -10,5 +11,6 @@ module.exports = () => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use("/api/events", eventRouter);
+  app.use("/api", authRouter);
   return app;
 };
